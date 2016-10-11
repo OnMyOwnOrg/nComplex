5025977d4b2eaaf1f6a5609c133ddb7e0d88d830
Tobias Döppes <tobias.doeppes@isis-papyrus.com>
2016-10-11 13:44:33 +0200

    project structure

---
+++ meta/header.php
@@ -0,0 +1,12 @@
+<head>
+	<meta charset="utf-8">
+	<title>Space-Game</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="js game beta">
+	<meta name="author" content="OnMyOnw.at">
+
+	<script type="text/javascript" src="jslibraries/json3.min.js"></script>
+	<script type="text/javascript" src="jslibraries/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="jslibraries/knockout-3.4.0.min.js"></script>
+	<script type="text/javascript" src="jslibraries/material.min.js"></script>
+</head>
---
+++ tools/grunt/Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass_globbing: {
+			js_game: {
+				options: {
+					useSingleQuotes: false,
+					signature: '// scss import collection'
+				},
+				files: {
+					'../../sass/imports.scss': '../../sass/dist/**/*.scss',
+				}
+			}
+		},
+		sass: {
+			dist: {
+				files: {
+					'../../css/dist/main.css' : '../../sass/imports.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 4 versions']
+			},
+			files: {
+				src: '../../css/dist/*.css'
+			}
+		},
+		watch: {
+			source: {
+				files: ['../../sass/dist/**/*.scss'],
+				tasks: ['sass_globbing','sass', 'autoprefixer'],
+				options: {
+					livereload: false // needed to run LiveReload
+				}
+			}
+		},
+		uglify: {
+	    all_src : {
+	      options : {
+	        sourceMap : false,
+	        sourceMapName : 'sourceMap.map'
+	      },
+	      src : '../../js/**/*.js',
+	      dest : '../../jslibraries/dist/composite.all.min.js'
+	    }
+	  }
+	});
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass-globbing');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('jscomp', ['uglify']);
+	grunt.registerTask('sasscomp', ['sass_globbing','sass', 'autoprefixer']);
+	grunt.registerTask('default',['watch']);
+};
